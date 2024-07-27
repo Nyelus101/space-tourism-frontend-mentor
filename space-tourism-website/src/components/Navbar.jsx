279419e621703b9400b92a82175013c3b543eb81
Nyelus101 <cmuonyelu@gmail.com>
2024-07-27 22:23:35 +0100

    update on navbar

--- space-tourism-website/src/components/Navbar.jsx
+++ space-tourism-website/src/components/Navbar.jsx
@@ -86,8 +86,10 @@ const Nav = () => {
       <div className='flex items-center justify-between p-5 w-357 h-88'>
         <div className='flex items-center'>
           <img src={logo} alt="Logo" className='w-10 h-10 md:w-12 md:h-12' />
+          <div className="hidden lg:block absolute w-[35%] h-[2px] right-[50%] z-[10] bg-white opacity-25 mix-blend-normal"></div>
         </div>
         
+        {/* <div className="bar grow hidden lg:block h-0.5  bg-white/70 translate-x-8 relative z-20"></div> */}
           <div onClick={handleNav} className="z-[3000] md:hidden">
             {nav ? (
               <img src={close} alt="Close" className="relative" style={{ width: '24px', height: '21px' }} />
@@ -96,14 +98,14 @@ const Nav = () => {
             )}
           </div>
         
-        <nav className={`${nav ? 'block' : 'hidden'} md:flex md:items-center md:space-x-8 md:relative md:top-0 md:right-0 md:w-auto md:bg-transparent absolute top-0 right-0 w-[254px] h-full bg-white z-[2000]`}>
-        <ul className='pl-[32px] space-y-8 relative top-[133px] md:top-0 md:pl-0 md:space-y-0 md:flex md:space-x-8'>
-          <li>00 HOME</li>
-          <li>01 DESTINATION</li>
-          <li>02 CREW</li>
-          <li>03 TECHNOLOGY</li>
-        </ul>
-      </nav>
+        <nav className={`${nav ? 'block' : 'hidden'} md:flex md:items-center md:space-x-8 md:relative md:top-0 md:right-[-2%] md:w-[736px] md:h-[96px] md:bg-transparent  absolute top-0 right-0 md:mr-0 w-[254px] h-full md:bg-white z-[2000] ml-[15%] bg-white/5 `}>
+          <ul className='pl-[32px] space-y-8 relative top-[133px] md:top-0 md:pl-0 md:left-[20%] md:space-y-0 md:flex md:space-x-8'>
+            <li>00 HOME</li>
+            <li>01 DESTINATION</li>
+            <li>02 CREW</li>
+            <li>03 TECHNOLOGY</li>
+          </ul>
+        </nav>
       </div>
     </>
   );
