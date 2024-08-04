793350440990d97b703316b334a2f888cdb64e5d
Nyelus101 <cmuonyelu@gmail.com>
2024-08-04 19:51:25 +0100

    update

--- space-tourism-website/src/components/Technology.jsx
+++ space-tourism-website/src/components/Technology.jsx
@@ -1,5 +1,46 @@
 import React, { useState } from 'react';
-import data from "../starter-code/data.json";
+// import data from "../starter-code/data.json";
+import '../index.css';
+
+import data from "../data.json"
+import vehicleLandscape from "../assets/technology/image-launch-vehicle-landscape.jpg";
+import vehiclePortrait from "../assets/technology/image-launch-vehicle-portrait.jpg";
+import capsuleLandscape from "../assets/technology/image-space-capsule-landscape.jpg";
+import capsulePortrait from "../assets/technology/image-space-capsule-portrait.jpg";
+import spaceportLandscape from "../assets/technology/image-spaceport-landscape.jpg";
+import spaceportPortrait from "../assets/technology/image-spaceport-portrait.jpg";
+
+
+// Data
+const technologies = [
+  {
+    name: "Launch vehicle",
+    landscape: vehicleLandscape,
+    portrait: vehiclePortrait,
+    description: `A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!`,
+  },
+  {
+    name: "Space capsule",
+    landscape: capsuleLandscape,
+    portrait: capsulePortrait,
+    description: `A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.`,
+  },
+  {
+    name: "Spaceport",
+    landscape: spaceportLandscape,
+    portrait: spaceportPortrait,
+    description: `A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.`,
+  },
+];
+
+
+
+// const imageMap = {
+//   'Moon': vehicleLandscape,
+//   'Mars': marsPng,
+//   'Europa': europaPng,
+//   'Titan': titanPng,
+// };
 
 const Technology = () => {
   const [technology] = useState(data.technology);
@@ -8,15 +49,15 @@ const Technology = () => {
   const { name, images, description } = technology[value];
 
   return (
-    <div className="bg-technology-sm md:bg-technology-md lg:bg-technology-lg flex flex-col items-center justify-center h-[100vh] p-10">
-      <section className="home technology">
+    <div className="bg-technology-sm md:bg-technology-md lg:bg-technology-lg lg:pl-44 p-0 m-0 h-full">
+      <div className="text-center md:text-left w-fit mx-auto mb-8 md:mx-2">
+        <h2 className="text-3xl font-extralight text-gray-300 uppercase tracking-wider pb-5 md:pb-10">
+          03
+          <span className="ml-5 font-bold">Space Launch 101</span>
+        </h2>
+      </div>
         <div className="px-5 pt-32 grid grid-cols-1 md:grid-cols-2 gap-10 xl:max-w-7xl xl:mx-auto">
-          <article className="text-center md:text-left">
-            <h2 className="text-3xl font-extralight text-gray-300 uppercase tracking-wider pb-5 md:pb-10">
-              03
-              <span className="ml-5 font-bold">Space Launch 101</span>
-            </h2>
-
+          <article>
             <img
               src={images.portrait}
               alt={name}
@@ -63,7 +104,6 @@ const Technology = () => {
             />
           </article>
         </div>
-      </section>
     </div>
   );
 };
